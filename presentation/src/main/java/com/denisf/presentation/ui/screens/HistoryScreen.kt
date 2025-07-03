f3aa3623dfeb2a3b6d4020d66c69f206fd11a975
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-07-03 12:03:32 +0300

    Порпавил RaceScreen и реализовал UI для HistoryScreen.

--- presentation/src/main/java/com/denisf/presentation/ui/screens/HistoryScreen.kt
+++ presentation/src/main/java/com/denisf/presentation/ui/screens/HistoryScreen.kt
@@ -1,48 +1,58 @@
 package com.denisf.presentation.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.denisf.domain.model.FinishedRace
+import com.denisf.presentation.ui.theme.PaleOrangeYellow
+import com.denisf.presentation.ui.theme.OrangeYellow
+import com.denisf.presentation.ui.theme.PaleGrayBrown
 
 @Composable
 fun HistoryScreen() {
     val winners = listOf<FinishedRace>(
-        FinishedRace(0, 1,"02.07.2025", 1, 2),
-        FinishedRace(1, 2,"02.07.2025", 4, 6),
-        FinishedRace(2, 3,"04.07.2025", 3, 2),
-        FinishedRace(3, 4,"02.07.2025", 2, 6),
-        FinishedRace(4, 5,"12.07.2025", 6, 4),
+        FinishedRace(0, 1, "02.07.2025", 1, 2),
+        FinishedRace(1, 2, "02.07.2025", 4, 6),
+        FinishedRace(2, 3, "04.07.2025", 3, 2),
+        FinishedRace(3, 4, "02.07.2025", 2, 6),
+        FinishedRace(4, 5, "12.07.2025", 6, 4),
     )
 
     LazyColumn(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxSize()
+            .background(PaleOrangeYellow),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
+            Spacer(modifier = Modifier.height(10.dp))
             Text(
                 text = "История забегов",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Medium
             )
+            Spacer(modifier = Modifier.height(10.dp))
         }
 
         items(winners) {
@@ -56,18 +66,29 @@ fun HistoryScreen() {
 
 @Composable
 private fun HistoryRaceItem(finishedRace: FinishedRace) {
-    Row(modifier = Modifier.fillMaxWidth().background(Color.LightGray), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(PaleGrayBrown.copy(alpha = 0.5f), RoundedCornerShape(10.dp))
+            .border(1.dp,OrangeYellow, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Text("№ ${finishedRace.numberRace}")
-        Column(verticalArrangement = Arrangement.SpaceEvenly) {
-            Text("Победила лошадь №${finishedRace.winner}")
-            Row {
-                Text("Забег прошел - ${finishedRace.date}")
-                Text("Всего участников - ${finishedRace.horsesCount}")
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
+            Text(
+                "Победила лошадь №${finishedRace.winner}",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium)
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                Text("Участников - ${finishedRace.horsesCount}")
+                Text("Дата - ${finishedRace.date}")
             }
         }
     }
 }
-
 
 @Preview
 @Composable
--- presentation/src/main/java/com/denisf/presentation/ui/screens/RaceScreen.kt
+++ presentation/src/main/java/com/denisf/presentation/ui/screens/RaceScreen.kt
@@ -1,12 +1,15 @@
 package com.denisf.presentation.ui.screens
 
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,16 +20,23 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,6 +44,11 @@ import androidx.compose.ui.unit.times
 import com.denisf.domain.model.Horse
 import com.denisf.presentation.ui.RaceStatus
 import com.denisf.presentation.ui.RaceViewModel
+import com.denisf.presentation.ui.theme.BrownYellowWiltedLeaves
+import com.denisf.presentation.ui.theme.DeepBrown
+import com.denisf.presentation.ui.theme.PaleGrayBrown
+import com.denisf.presentation.ui.theme.PaleOrangeYellow
+import com.denisf.presentation.ui.theme.ShinyYellow
 
 @Composable
 fun RaceScreen(viewModel: RaceViewModel) {
@@ -49,14 +64,20 @@ fun RaceScreen(viewModel: RaceViewModel) {
     val raceLengthText by viewModel.raceLengthText.collectAsState()
     val isValidRaceLength by viewModel.isValidRaceLength.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PaleOrangeYellow),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(400.dp),
+                .height(480.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (raceStatus != RaceStatus.START) {
+                Spacer(modifier = Modifier.height(20.dp))
                 when {
                     race?.isFinished == true -> {
                         Text(
@@ -80,7 +101,8 @@ fun RaceScreen(viewModel: RaceViewModel) {
                 Row(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(8.dp),
+                        .padding(8.dp)
+                        .background(BrownYellowWiltedLeaves, RoundedCornerShape(10.dp)),
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     race?.horses?.forEach { horse ->
@@ -96,31 +118,64 @@ fun RaceScreen(viewModel: RaceViewModel) {
                     }
                 }
             } else {
-                TextField(
-                    value = horseCountText,
-                    onValueChange = { viewModel.updateHorseCountText(it) },
-                    label = { Text(text = "Введите количество лошадей (2-6)") },
-                    singleLine = true,
-                    isError = horseCountText.isNotBlank() && !isValidHorseCount,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 30.dp),
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                TextField(
-                    value = raceLengthText,
-                    onValueChange = {
-                        viewModel.updaterRaceLengthText(it)
-                    },
-                    label = { Text(text = "Введите длину забега") },
-                    singleLine = true,
-                    isError = raceLengthText.isNotBlank() && !isValidRaceLength,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 30.dp),
-                )
+                        .fillMaxHeight(.75f)
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    TextField(
+                        value = horseCountText,
+                        onValueChange = { viewModel.updateHorseCountText(it) },
+                        label = { Text(text = "Введите количество лошадей (2-6)") },
+                        singleLine = true,
+                        isError = horseCountText.isNotBlank() && !isValidHorseCount,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            unfocusedContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            errorContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            focusedLabelColor = Color.DarkGray,
+                            unfocusedLabelColor = Color.DarkGray,
+                            errorLabelColor = Color.Red,
+                            focusedIndicatorColor = Color.DarkGray,
+                            unfocusedIndicatorColor = Color.LightGray,
+                            errorIndicatorColor = Color.Red,
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.Black
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp),
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    TextField(
+                        value = raceLengthText,
+                        onValueChange = {
+                            viewModel.updaterRaceLengthText(it)
+                        },
+                        label = { Text(text = "Введите длину забега") },
+                        singleLine = true,
+                        isError = raceLengthText.isNotBlank() && !isValidRaceLength,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            unfocusedContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            errorContainerColor = PaleGrayBrown.copy(alpha = 0.5f),
+                            focusedLabelColor = Color.DarkGray,
+                            unfocusedLabelColor = Color.DarkGray,
+                            errorLabelColor = Color.Red,
+                            focusedIndicatorColor = Color.DarkGray,
+                            unfocusedIndicatorColor = Color.LightGray,
+                            errorIndicatorColor = Color.Red,
+                            focusedTextColor = Color.Black,
+                            unfocusedTextColor = Color.Black
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 30.dp),
+                    )
+                }
             }
         }
 
@@ -136,25 +191,41 @@ fun RaceScreen(viewModel: RaceViewModel) {
                                 trackLength = viewModel.raceLength.value
                             )
                         }
-                    }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = PaleGrayBrown,
+                        contentColor = Color.White
+                    )
                 ) {
                     Text("Старт")
                 }
             }
 
             RaceStatus.FINISHED -> {
-                Button(onClick = {
-                    viewModel.updateRaceStatus(RaceStatus.START)
-                    viewModel.clearInput()
-                }) {
+                Button(
+                    onClick = {
+                        viewModel.updateRaceStatus(RaceStatus.START)
+                        viewModel.clearInput()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = PaleGrayBrown,
+                        contentColor = Color.White
+                    )
+                ) {
                     Text("Заново")
                 }
             }
 
             RaceStatus.RUNNING -> {
-                Button(onClick = {
-                    viewModel.stopRace()
-                }) {
+                Button(
+                    onClick = {
+                        viewModel.stopRace()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = PaleGrayBrown,
+                        contentColor = Color.White
+                    )
+                ) {
                     Text("Завершить забег")
                 }
             }
@@ -165,40 +236,78 @@ fun RaceScreen(viewModel: RaceViewModel) {
 @Composable
 fun HorseTrackVertical(
     horse: Horse,
-    trackHeight: Dp = 400.dp,
+    trackHeight: Dp = 375.dp,
     maxProgress: Int,
     winner: Boolean
 ) {
-    val progressFraction = (horse.progress / maxProgress).coerceIn(0f, 1f)
-    val animatedOffset by animateDpAsState(
-        targetValue = (1f - progressFraction) * trackHeight
+    val rawProgressFraction = (horse.progress / maxProgress).coerceIn(0f, 1f)
+
+    val animatedProgressFraction by animateFloatAsState(
+        targetValue = rawProgressFraction,
+        animationSpec = tween(durationMillis = 120),
     )
 
-    Box(
-        modifier = Modifier
-            .width(10.dp)
-            .height(trackHeight)
-            .background(
-                if (winner) Color.Green else Color.LightGray,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(4.dp),
-        contentAlignment = Alignment.TopCenter
+    val animatedHorseFraction by animateDpAsState(
+        targetValue = (1f - animatedProgressFraction) * (trackHeight),
+        animationSpec = tween(durationMillis = 0),
+    )
+    val adjustedOffset = animatedHorseFraction - 50.dp
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.width(60.dp)
     ) {
+        Text(
+            text = horse.name,
+            fontSize = 12.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         Box(
             modifier = Modifier
-                .offset(y = animatedOffset)
-                .size(32.dp),
-            contentAlignment = Alignment.Center
+                .width(60.dp)
+                .height(trackHeight)
         ) {
-            Text("🐎", fontSize = 26.sp)
+            Box(
+                modifier = Modifier
+                    .width(10.dp)
+                    .height(trackHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(RoundedCornerShape(8.dp))
+            ) {
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .background( Color.LightGray)
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(animatedProgressFraction)
+                        .align(Alignment.BottomCenter)
+                        .background(if (winner) ShinyYellow else Color.Gray)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .offset(y = adjustedOffset)
+                    .align(Alignment.TopCenter)
+                    .width(60.dp)
+                    .height(100.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("🐎", fontSize = 30.sp)
+            }
         }
     }
-
-    Text(
-        text = horse.name,
-        fontSize = 12.sp
-    )
 }
 
 //@Preview
